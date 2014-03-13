ea1f87addace242e770481e88b25725d65b155ea
Anne-Laure Pequegnot <al.pequegnot@ipnl.in2p3.fr>
2014-03-13 14:07:12 +0100

    adapt the executables according to the new names

--- dataMcComparison/runTheCode_woPU.py
+++ dataMcComparison/runTheCode_woPU.py
@@ -2,6 +2,6 @@
 
 import os, subprocess, datetime, tempfile
 
-cmd = "./dataMcComparison ../analysis/output_rootfile/19Feb14/data/MULTIJET_Run2012ABCD-22Jan2013_analysis_woPU_pt25_eta50_puJetIdMT_HLTsel_woPtRecoilCut.root ../analysis/output_rootfile/19Feb14/MC/MULTIJET_MC_QCD_HT-500ToInf_analysis_woPU_pt25_eta50_puJetIdMT_HLTsel_woPtRecoilCut.root --lumi --shape --lin --rmPU --plotName \"_pt25_eta50_puJetIdMT_HLTsel_woPtRecoilCut\" --extension \".pdf\""
+cmd = "./dataMcComparison ../analysis/output_rootfile/12Mar14/data/MULTIJET_Run2012ABCD-22Jan2013_analysis_woPU_pt25_eta50_puJetIdMT_HLTsel_woPtRecoilCut.root ../analysis/output_rootfile/12Mar14/MC/MULTIJET_MC_QCD_Pt-120To1400_pythia_analysis_woPU_pt25_eta50_puJetIdMT_HLTsel_woPtRecoilCut.root --lumi --shape --lin --rmPU --plotName \"_pt25_eta50_puJetIdMT_HLTsel_woPtRecoilCut_QCD-Pt-pythia\" --extension \".pdf\""
 os.system(cmd);
 
