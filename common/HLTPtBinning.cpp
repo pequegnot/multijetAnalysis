518659acf5cadda8fc81d8d97585ce8d82a0d476
Anne-Laure Pequegnot <al.pequegnot@ipnl.in2p3.fr>
2014-04-18 09:32:22 +0200

    Replace the bin with 350 by 360

--- common/HLTPtBinning.cpp
+++ common/HLTPtBinning.cpp
@@ -15,8 +15,8 @@ void HLTPtBinning::fillHLTPtBins()
   mPtBins.clear();
   mPtBins.push_back(std::make_pair(170., 230.));
   mPtBins.push_back(std::make_pair(230., 290.));
-  mPtBins.push_back(std::make_pair(290., 350.));
-	mPtBins.push_back(std::make_pair(350., 3000.));
+  mPtBins.push_back(std::make_pair(290., 360.));
+  mPtBins.push_back(std::make_pair(360., 3000.));
 }
 
 
