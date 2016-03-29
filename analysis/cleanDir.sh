a602c8b6c705fe90aaa9c7f44b017e99911dc27b
Anne-Laure Pequegnot <a-l.pequegnot@ipnl.in2p3.fr>
2016-03-29 16:36:28 +0200

    Script to clean directories containing plots

---
+++ analysis/cleanDir.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm ./images/MJB/*
+rm images/MJB_gen/*
+rm images/MPF/*
+rm images/MPF_gen/*
+rm images/Njets/*
+rm images/NjetsRecoil/*
+rm images/PtRatio/*
+rm images/Rrecoil/*
+rm images/Rtrue/*
+
---
+++ dataMcComparison/cleanDir.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+rm images/cExp/*
+rm images/LeadingJetPtperPtHLT/*
+rm images/LeadingJetPtperRefObjPt/*
+rm images/MJBperNpv/*
+rm images/MJBperRefObjEta/*
+rm images/MJBperRefObjPt/*
+rm images/MPFperRefObjPt/*
+rm images/NjetsRecoil/*
+rm images/recoil/*
+rm images/RecoilPtperRefObjPt/*
+rm images/response/*
+rm images/variables/*
+
