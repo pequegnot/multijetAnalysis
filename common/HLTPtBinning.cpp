197fc41754c4b7d54692fe5706eac847f451d899
Anne-Laure Pequegnot <a-l.pequegnot@ipnl.in2p3.fr>
2016-03-29 16:35:24 +0200

    Tune pt binning for 76X samples (QCD_HT_300to500 sample not available)

--- common/HLTPtBinning.cpp
+++ common/HLTPtBinning.cpp
@@ -22,26 +22,29 @@ void HLTPtBinning::fillHLTPtBins(bool useRecoilPtBinHLTSel)
     mPtBins.push_back(std::make_pair(170., 230.));
     mPtBins.push_back(std::make_pair(230., 290.));
     mPtBins.push_back(std::make_pair(290., 360.));
-    mPtBins.push_back(std::make_pair(360., 3000.));
+    mPtBins.push_back(std::make_pair(360., 440.));
+    mPtBins.push_back(std::make_pair(440., 3000.));
   } else {
-/*    mPtBins.push_back(std::make_pair(210., 280.));*/
-    //mPtBins.push_back(std::make_pair(280., 320.));
-    //mPtBins.push_back(std::make_pair(320., 400.));
-    /*mPtBins.push_back(std::make_pair(400., 3000.));*/
-    mPtBins.push_back(std::make_pair(210., 290.));
-    mPtBins.push_back(std::make_pair(290., 370.));
+    // QCD_HT_300to500 sample not available: starting the analysis at HT = 500 so pt recoil ~350-400
+/*    mPtBins.push_back(std::make_pair(210., 290.));*/
+    /*mPtBins.push_back(std::make_pair(290., 370.));*/
     mPtBins.push_back(std::make_pair(370., 470.));
-    mPtBins.push_back(std::make_pair(470., 3000.));
+    mPtBins.push_back(std::make_pair(470., 550.));
+    mPtBins.push_back(std::make_pair(550., 610.));
+    mPtBins.push_back(std::make_pair(610., 3000.));
   }
 }
 
 void HLTPtBinning::fillHLTName() {
 
   mPtNames.clear();
-  mPtNames.push_back("HLT_PFJet140");
-  mPtNames.push_back("HLT_PFJet200");
+  // QCD_HT_300to500 sample not available: starting the analysis at HT = 500 so pt recoil ~350-400
+/*  mPtNames.push_back("HLT_PFJet140");*/
+  /*mPtNames.push_back("HLT_PFJet200");*/
   mPtNames.push_back("HLT_PFJet260");
   mPtNames.push_back("HLT_PFJet320");
+  mPtNames.push_back("HLT_PFJet400");
+  mPtNames.push_back("HLT_PFJet450");
 
 }
 
